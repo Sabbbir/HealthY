afe2243ea16563b29156841cbcbf8f1f741cb520
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2022-12-02 00:36:55 +0600

    okay-okay

--- app/src/main/java/com/example/healthy/MainActivity.kt
+++ app/src/main/java/com/example/healthy/MainActivity.kt
@@ -3,20 +3,21 @@ package com.example.healthy
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+      //  startActivity(Intent(this, Register_Activity::class.java))
 
-        workout_Login_cred_submit.setOnClickListener {
-            val login_submit = Intent(this, MenuPage::class.java)
-            startActivity(login_submit)
-        }
-        workout_sign_up.setOnClickListener {
-            val sign_up = Intent(this, SignUp::class.java)
-            startActivity(sign_up)
-        }
-    }
+//        workout_Login_cred_submit.setOnClickListener {
+//            val login_submit = Intent(this, MenuPage::class.java)
+//            startActivity(login_submit)
+//        }
+//        workout_sign_up.setOnClickListener {
+//            val sign_up = Intent(this, SignUp::class.java)
+//            startActivity(sign_up)
+//        }
+  }
 }
